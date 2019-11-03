d9e3b26f6412b2a5cd1129b5e2f30a1529b62b85
Saussereau Dominique <domsau2@yahoo.fr>
2019-11-03 21:06:28 +0100

    Colors for windows and Linux !
    No color with clog in GNU/Linux!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 using namespace std;
 
-/*******************************************
-//  BUG of C++:                            /
-// This principle DO NOT work with clog!   /
-*******************************************/
+//*****************************************************//
+//  BUG of C++:                                        //
+// This principle DO NOT work with clog in GNU/Linux!  //
+//*****************************************************//
+#ifdef _WIN32
 
+void iout ( string message = "",  int colorisationFin = 7, int colorisation1 = 4, int colorisation2 = 31 ){ // Ok.
+            HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE); // just once
+     /* W   http://cppqa.blogspot.com/2013/11/colour-coding-in-c-first-time-i-saw.html
+        1_____Blue                                       10______Light Green
+2_____Green                                    11______Light Aqua
+3_____Aqua                                      12______Light Red
+4_____Red                                        13______Light Purple
+5_____Purple                                   14______Light Yellow
+6_____Yellow                                   15______Bright White
+7_____White
+8_____Gray
+9_____Light Blue
+0_____Black
+
+        */
+
+               SetConsoleTextAttribute(color, colorisation1);
+       cout << message;
+
+               SetConsoleTextAttribute(color, colorisationFin);
+}
+
+void ierr ( string message = "",  int colorisationFin = 7, int colorisation1 = 4, int colorisation2 = 31 ){ // Ok.
+     HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE); // just once
+               SetConsoleTextAttribute(color, colorisation1);
+       cerr << message;
+               SetConsoleTextAttribute(color, colorisationFin);
+}
+void ilog ( string message = "",  int colorisationFin = 7, int colorisation1 = 4, int colorisation2 = 31 ){ // Ok.
+     HANDLE color=GetStdHandle(STD_OUTPUT_HANDLE); // just once
+               SetConsoleTextAttribute(color, colorisation1);
+       clog << message;
+               SetConsoleTextAttribute(color, colorisationFin);
+}
+#else
 void iout ( string message = "",  int colorisationFin = 0, int colorisation1 = 1, int colorisation2 = 31 ) // Ok.
 {
     // Linux:
@@ -16,19 +55,10 @@ void iout ( string message = "",  int colorisationFin = 0, int colorisation1 = 1
     // 0 = normal.
     // 2 effects or color only.
     // 41 = red background...
-
-#ifdef _WIN32
-
-#else
     printf ( "\033[%dm", colorisation1 );
     printf ( "\033[%dm", colorisation2 );
-#endif
     cout << message;  // http://www.cplusplus.com/forum/beginner/108474/
-#ifdef _WIN32
-
-#else
     printf ( "\033[%dm", colorisationFin );
-#endif
 }
 
 void ierr ( string message = "",  int colorisationFin = 0, int colorisation1 = 1, int colorisation2 = 31 ) // Ok.
@@ -41,28 +71,30 @@ void ierr ( string message = "",  int colorisationFin = 0, int colorisation1 = 1
     // 0 = normal.
     // 2 effects or color only.
     // 41 = red background...
-#ifdef _WIN32
-
-#else
     printf ( "\033[%dm", colorisation1 );
     printf ( "\033[%dm", colorisation2 );
-#endif
     cerr << message;  // http://www.cplusplus.com/forum/beginner/108474/
-#ifdef _WIN32
-
-#else
     printf ( "\033[%dm", colorisationFin );
-#endif
 }
 
+void ilog(string message = "",  int colorisationFin = 0, int colorisation1 = 1, int colorisation2 = 31){ // No color with clog in GNU/Linux!
+clog << message << endl;
+}
+
+#endif
 int main()
 {
-    cout << "Common Output!" << endl;
+cout << "Common Output!" << endl;
 iout ( "Important Output!\n");
 cout << "Common Output!" << endl;
 
 cerr << "Common Error!" << endl;
 ierr ( "Important Error!\n");
 cerr << "Common Error!" << endl;
+
+clog << "Common Log!" << endl;
+ilog ( "Important Log!\n");
+clog << "Common Log!" << endl;
+
     return 0;
 }
